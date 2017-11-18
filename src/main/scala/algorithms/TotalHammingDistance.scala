37b5ad471aea960ec0eb8e54b488134f420bb1e4
Sili Wen <siliwen@gmail.com>
2017-11-18 15:34:43 -0800

    Timeout on hamming

--- src/main/scala/algorithms/TotalHammingDistance.scala
+++ src/main/scala/algorithms/TotalHammingDistance.scala
@@ -3,7 +3,21 @@ package algorithms
 object TotalHammingDistance {
 
   def totalHammingDistance(nums: Array[Int]): Int = {
-    0
+    val len = nums.length
+    var dist = 0
+    for (i <- 0 until len - 1) {
+      val a = nums(i)
+
+        for (j <- i+1 until len) {
+          val b = nums(j)
+          if (a != b) {
+            println(a + ", " + b + ": " + getHammingDist(a, b))
+            dist += getHammingDist(a, b)
+          }
+      }
+    }
+
+    dist
   }
 
   private def getHammingDist(a: Int, b: Int): Int = {
@@ -11,8 +25,12 @@ object TotalHammingDistance {
     var cnt = 0
 
     while (xor > 0) {
-      val mask = (xor & (xor-1))
-      xor >> 2
+      val mask = (xor & 1)
+      if ((xor & mask) == 1) {
+        cnt += 1
+      }
+
+      xor = (xor >> 1)
     }
 
     cnt
@@ -20,7 +38,7 @@ object TotalHammingDistance {
 
 
   def main(args: Array[String]): Unit = {
-    val dist = getHammingDist(2, 6)
-    println(dist)
+    val nums = Array(4, 14, 4, 14)
+    totalHammingDistance(nums)
   }
 }
