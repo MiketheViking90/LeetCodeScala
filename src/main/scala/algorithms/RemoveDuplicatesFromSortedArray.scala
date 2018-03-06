b5feb9d00636cad648364f125870427f906986f3
Sili Wen <siliwen@amazon.com>
2018-03-06 11:32:43 -0800

    Start remove duples

---
+++ src/main/scala/algorithms/RemoveDuplicatesFromSortedArray.scala
@@ -0,0 +1,22 @@
+package algorithms
+
+object RemoveDuplicatesFromSortedArray {
+  def removeDuplicates(nums: Array[Int]): Int = {
+    if (nums == null || nums.length == 0) {
+      return 0
+    }
+    if (nums.length == 1) {
+      return 1
+    }
+
+    val len = nums.length
+    var cnt = 1
+    var i = 0
+    var j = 0
+
+    while (j < len) {
+    }
+
+    cnt
+  }
+}
